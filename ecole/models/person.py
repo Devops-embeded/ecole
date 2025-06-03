d1a91580286533ff357580c45174d5b010fda917
Devops-embeded <enzomagnier30@gmail.com>
2025-06-03 15:00:33 +0200

    add file

--- ecole/models/person.py
+++ ecole/models/person.py
@@ -5,14 +5,12 @@ from typing import Optional
 from .address import Address
 
 
-
 @dataclass
 class Person(ABC):
     first_name: str
     last_name: str
     age: int
     address: Optional[Address] = field(default=None, init=False)
-
     def __str__(self) -> str:
         return f"{self.first_name} {self.last_name} ({self.age} ans)" + \
                (f", {self.address}" if self.address else "")
